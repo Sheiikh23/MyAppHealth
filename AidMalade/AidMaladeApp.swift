229ded117941491b25853af2a5760f0e1febcc22
Cheikh Tacko SECK <cheikhseck24@gmail.com>
2022-01-25 19:12:28 +0100

    Initial Commit

---
+++ AidMalade/AidMaladeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AidMaladeApp.swift
+//  AidMalade
+//
+//  Created by Cheikh SECK on 25/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AidMaladeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AidMalade/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AidMalade
+//
+//  Created by Cheikh SECK on 25/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
